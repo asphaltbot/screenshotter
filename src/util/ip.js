8f6c9986ead4fbe4cf8548fc378d2d5b273f0ea4
Connor Wright <connor@connorwright.uk>
2020-05-27 17:34:15 +0100

    try actually using ipv6 to make the request (i should probably make a new branch)

--- src/util/ip.js
+++ src/util/ip.js
@@ -14,7 +14,7 @@ module.exports = {
 
     getIPv6: function () {
         return new Promise(function (resolve, reject) {
-            request("GET", "https://api6.ipify.org?format=json")
+            request("GET", "https://api6.ipify.org?format=json", {family: 6})
                 .getBody("utf-8")
                 .then(JSON.parse)
                 .done(function (res) {
